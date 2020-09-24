6f814bbe71f59538c0a47604b3753efd04a658d9
tabishadnan <tabishadnan000@gmail.com>
2020-09-24 12:08:30 -0700

    React Js Looping Is Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import User from './User';
 
 function App() {
   return (
     <div className="App">
-      
+      <User name="Tabish Adnan" age={20} education="Intermediate" hobbies={["Coding", "Playing", "Reading Books"]} />
+      <User name="Muneeb Khan" age={22} education="BSCS" hobbies={["Chess", "Archery", "Paintball", "Photography"]} />
     </div>
   );
 }
